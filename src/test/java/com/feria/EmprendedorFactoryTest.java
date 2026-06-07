fb13bfb49956819d9ed69bb2432b1a4eba0603ec
Christian <72816825+ccettour@users.noreply.github.com>
2026-06-07 20:52:00 -0300

    Actualiza pom.xml y agrega EmprendedorFactoryTest

---
+++ src/test/java/com/feria/EmprendedorFactoryTest.java
@@ -0,0 +1,14 @@
+package com.feria;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EmprendedorFactoryTest {
+
+    @Test
+    void entornoConfiguradoCorrectamente() {
+        assertTrue(true);
+    }
+
+}
